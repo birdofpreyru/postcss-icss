00dfa0fad3e9133fe98a10ce188709ccd9bce2dd
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2026-07-24 18:51:33 +0200

    v1.3.4: Update of dependencies

--- config/babel/preset.js
+++ config/babel/preset.js
@@ -2,9 +2,6 @@ export default function preset(api, options) {
   let envPreset = '@babel/env';
   if (options) envPreset = [envPreset, options];
   return {
-    plugins: [
-      '@babel/plugin-transform-runtime',
-    ],
     presets: [
       envPreset,
       '@babel/typescript',
